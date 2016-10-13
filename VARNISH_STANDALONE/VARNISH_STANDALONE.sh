3bc0a6599418479775fa19aea94ed85210b86c20
Ashijit Pramanik <ashijit007@gmail.com>
2016-10-13 20:36:47 +0530

    specifying HOME as /root/ to check if it fixes OSError: [Errno 30] Read-only file system: '/home/ashijit'

--- VARNISH_STANDALONE/VARNISH_STANDALONE.sh
+++ VARNISH_STANDALONE/VARNISH_STANDALONE.sh
@@ -2,5 +2,7 @@
 
 APP=`grep APP /tmp/env | cut -d'=' -f2`
 
+export HOME=/root/
+export ANSIBLE_REMOTE_TMP=$HOME/.ansible/tmp
 ansible-galaxy install -r /root/scripts/$APP/"$APP"_requirements.yml
 ansible-playbook /root/scripts/$APP/"$APP"_playbook.yml
