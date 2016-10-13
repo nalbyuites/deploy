6b1a90ebc01fb742145e952814e8df226df7bf0a
Ashijit Pramanik <ashijit007@gmail.com>
2016-10-13 19:39:39 +0530

    corrected path of playbook and removed test touch commands

--- VARNISH_STANDALONE/VARNISH_STANDALONE.sh
+++ VARNISH_STANDALONE/VARNISH_STANDALONE.sh
@@ -2,7 +2,5 @@
 
 APP=`grep APP /tmp/env | cut -d'=' -f2`
 
-touch /root/1.txt
 ansible-galaxy install -r /root/scripts/$APP/"$APP"_requirements.yml
-ansible-playbook /etc/ansible/roles/$APP/"$APP"_playbook.yml
-touch /root/2.txt
+ansible-playbook /root/scripts/$APP/"$APP"_playbook.yml
