e91b88b175bab258578feab65b9595d20b0d5938
Ashijit Pramanik <ashijit007@gmail.com>
2016-10-13 19:23:55 +0530

    bash variable put in double quotes for reference

--- VARNISH_STANDALONE/VARNISH_STANDALONE.sh
+++ VARNISH_STANDALONE/VARNISH_STANDALONE.sh
@@ -3,6 +3,6 @@
 APP=`grep APP /tmp/env | cut -d'=' -f2`
 
 touch /root/1.txt
-ansible-galaxy install -r /root/scripts/$APP/$APP_requirements.yml
-ansible-playbook /etc/ansible/roles/$APP/$APP_playbook.yml
+ansible-galaxy install -r /root/scripts/$APP/"$APP"_requirements.yml
+ansible-playbook /etc/ansible/roles/$APP/"$APP"_playbook.yml
 touch /root/2.txt
--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 
 /root/scripts/vmcontext start
 APP=`grep APP /tmp/env | cut -d'=' -f2`
-/root/scripts/$APP/$APP.sh
+/root/scripts/$APP/"$APP".sh
 
 rm -f /tmp/env
