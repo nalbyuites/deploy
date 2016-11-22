dfc0968859443bfae307aad70ff76daba0d4bd4a
Ashijit Pramanik <ashijit007@gmail.com>
2016-11-23 03:06:08 +0530

    corrected typo

--- VARNISH_STANDALONE/VARNISH_STANDALONE.sh
+++ VARNISH_STANDALONE/VARNISH_STANDALONE.sh
@@ -24,7 +24,7 @@ echo "varnish_default_backend_host: '$VARNISH_DEFAULT_BACKEND_HOST'" >> /etc/ans
 echo "varnish_default_backend_port: '$VARNISH_DEFAULT_BACKEND_PORT'" >> /etc/ansible/roles/$APP/defaults/main.yml
 
 # Install varnish the first time and only configure varnish thereafter.
-if ! type "varnishd" > /dev/null; then
+if type "varnishd" > /dev/null; then
 
     ansible-playbook /root/scripts/$APP/"$APP"_playbook.yml --tags "varnish-configure,varnish-service"
 
